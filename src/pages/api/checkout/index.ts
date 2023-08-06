2c59c2e98a0e276b622403134446a42893d8d329
kane <zeropointdigit@gmail.com>
2023-08-06 02:26:45 -0400

    update shipping addy countries

--- src/pages/api/checkout/index.ts
+++ src/pages/api/checkout/index.ts
@@ -22,7 +22,7 @@ async function createCheckoutSession(lineItems: Stripe.Checkout.SessionCreatePar
     success_url: 'https://primeorca.com/success?session_id={CHECKOUT_SESSION_ID}',
     cancel_url: 'https://primeorca.com',
     shipping_address_collection: {
-      allowed_countries: ['US', 'JP', 'GB', 'FR', 'SG', 'CA'],
+      allowed_countries: ['US', 'JP', 'GB', 'FR', 'SG', 'CA', 'CN', 'CO', 'EG', 'DE', 'HK', 'IT', 'KR', 'MO', 'MX', 'NG', 'ES', 'TW'],
     },
     // invoice_creation: {
     // enabled: true,
