e4f030d7f7a85c030d12d982018fc861aca9a375
kane <zeropointdigit@gmail.com>
2023-08-18 12:25:06 -0400

    video added to process page

--- src/components/common/hero.tsx
+++ src/components/common/hero.tsx
@@ -1,5 +1,5 @@
-import VideoThumb from '@/public/images/hero-image-01.jpg'
-// import ModalVideo from '@/components/modal-video'
+import VideoThumb from './public/assets/assets/images/Process-Video-Thumbnail.png'
+import ModalVideo from './modal-video'
 import Image from 'next/image'
 
 export default function Hero() {
@@ -7,21 +7,30 @@ export default function Hero() {
         <section>
             {/* <div className="max-w-6xl mx-auto px-4 sm:px-6 relative"> */}
 
-            <div
-                className="flex justify-center p-6 md:p-10 2xl:p-8 relative bg-no-repeat bg-center bg-cover"
-                style={{
-                    backgroundImage: 'url(/assets/images/features/header.png)',
-                }}
-            >
-                <div className="absolute top-0 ltr:left-0 rtl:right-0 bg-black w-full h-full opacity-50 transition-opacity duration-500 group-hover:opacity-80" />
-                <div className="w-full flex items-center justify-center relative z-10 py-10 md:py-14 lg:py-20 xl:py-24 2xl:py-32">
-                    <h2 className="text-xl md:text-2xl lg:text-3xl font-bold text-white text-center">
-                        Our Process
-                    </h2>
+            <div>
+                <div
+                    className="flex justify-center p-6 md:p-10 2xl:p-8 relative bg-no-repeat bg-center bg-cover mb-14"
+                    style={{
+                        backgroundImage: 'url(/assets/images/features/header.png)',
+                    }}
+                >
+                    <div className="absolute top-0 ltr:left-0 rtl:right-0 bg-black w-full h-full opacity-50 transition-opacity duration-500 group-hover:opacity-80" />
+                    <div className="w-full flex items-center justify-center relative z-10 py-10 md:py-14 lg:py-20 xl:py-24 2xl:py-32">
+                        <h2 className="text-xl md:text-2xl lg:text-3xl font-bold text-white text-center">
+                            Our Process
+                        </h2>
+                    </div>
                 </div>
-            </div>
 
-            {/* </div> */}
+                <ModalVideo
+                    thumb='https://po-prod.s3.us-west-1.amazonaws.com/_app/Process-Video-Thumbnail.png'
+                    thumbWidth={1024}
+                    thumbHeight={576}
+                    thumbAlt="Modal video thumbnail"
+                    video="https://www.youtube.com/embed/qvCFHpb_EYY?si=btlT2mxtU5ebdyIz"
+                    videoWidth={1920}
+                    videoHeight={1080} />
+            </div>
         </section>
     )
 }
--- src/components/common/zigzag.tsx
+++ src/components/common/zigzag.tsx
@@ -25,7 +25,7 @@ export default function Zigzag() {
                         <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
                             {/* Image */}
                             <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
-                                <Image className="max-w-full mx-auto md:max-w-none h-auto" src={'https://po-prod.s3.us-west-1.amazonaws.com/features/factory.png'} width={540} height={405} alt="Features 01" />
+                                <Image className="max-w-full mx-auto md:max-w-none h-auto" src={'https://po-prod.s3.us-west-1.amazonaws.com/features/feat-1.png'} width={540} height={405} alt="Features 01" />
                             </div>
                             {/* Content */}
                             <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
@@ -104,7 +104,7 @@ export default function Zigzag() {
                         <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
                             {/* Image */}
                             <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
-                                <Image className="max-w-full mx-auto md:max-w-none h-auto" src={'https://po-prod.s3.us-west-1.amazonaws.com/features/feat-2.png'} width={540} height={405} alt="Features 03" />
+                                <Image className="max-w-full mx-auto md:max-w-none h-auto" src={'https://po-prod.s3.us-west-1.amazonaws.com/features/constructive-design.png'} width={540} height={405} alt="Features 03" />
                             </div>
                             {/* Content */}
                             <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
