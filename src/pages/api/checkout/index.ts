0e0f13a69b2097767256c99a5023cb1bd250d85c
kane <zeropointdigit@gmail.com>
2023-08-25 02:08:47 -0400

    coupon code added

--- src/pages/api/checkout/index.ts
+++ src/pages/api/checkout/index.ts
@@ -33,6 +33,7 @@ async function createCheckoutSession(lineItems: Stripe.Checkout.SessionCreatePar
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card', 'cashapp'], // 'google_pay', 'apple_pay'],
     mode: 'payment',
+    allow_promotion_codes: true,
     line_items: lineItems,
     success_url: 'https://primeorca.com/success',
     // success_url: 'https://primeorca.com/success?session_id={CHECKOUT_SESSION_ID}',
