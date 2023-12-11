ceedec91329395d3dad21655c6f05db1f37c50fb
kane <zeropointdigit@gmail.com>
2023-12-11 17:39:22 -0500

    update product/[slug] pages - slug title

--- src/pages/McQ/[slug].tsx
+++ src/pages/McQ/[slug].tsx
@@ -49,7 +49,7 @@ export default function Collections({ slug, }: InferGetStaticPropsType<typeof ge
                     </div>
                     <div className="w-full ltr:lg:-ml-9 rtl:lg:-mr-9">
                         {/* <CollectionTopBar /> */}
-                        {/* <h1 className="text-lg font-bold md:text-xl lg:text-2xl text-heading">{slug.charAt(0).toUpperCase() + slug.slice(1)}</h1> */}
+                        {/* <h1 className="text-lg font-bold md:text-xl lg:text-2xl text-heading">{slug}</h1> */}
                         <ProductGrid slug={slug} />
                     </div>
                 </div>
--- src/pages/Nke/[slug].tsx
+++ src/pages/Nke/[slug].tsx
@@ -48,7 +48,8 @@ export default function Collections({ slug, }: InferGetStaticPropsType<typeof ge
 
                     <div className="w-full ltr:lg:-ml-9 rtl:lg:-mr-9">
                         {/* <CollectionTopBar /> */}
-                        <h1 className="text-lg font-bold md:text-xl lg:text-2xl text-hea    ding">{slug.charAt(0).toUpperCase() + slug.slice(1)}</h1>
+                        {/* <h1 className="text-lg font-bold md:text-xl lg:text-2xl text-heading">{slug.charAt(0).toUpperCase() + slug.slice(1)}</h1> */}
+                        <h1 className="text-lg font-bold md:text-xl lg:text-2xl text-heading">{slug}</h1>
                         <ProductGrid slug={slug} />
                     </div>
                 </div>
