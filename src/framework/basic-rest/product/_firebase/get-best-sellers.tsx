88b1acab3f68dae6d679efcdbcc82074275d8e44
kane <kanema20@gmail.com>
2023-12-20 14:32:49 -0500

    update order by desc

--- src/framework/basic-rest/product/_firebase/get-best-sellers.tsx
+++ src/framework/basic-rest/product/_firebase/get-best-sellers.tsx
@@ -9,14 +9,15 @@ import {
     QuerySnapshot,
     DocumentData,
     where,
-    getDocs
+    getDocs,
+    orderBy
 } from "firebase/firestore";
 import { firestore } from '@firebaseQueries/app';
 
 const fetchBestSellers = async ({ queryKey }: any) => {
     const [_key, _params] = queryKey;
 
-    const query_ref = getDocs(query(collection(firestore, "/products"), where("metadata_.type", "==", "Sample")));
+    const query_ref = getDocs(query(collection(firestore, "/products"), where("metadata_.type", "==", "Sample"), orderBy("_id", "desc")));
 
     let query_data: any = [];
     const snapshot = (await query_ref).docs;
